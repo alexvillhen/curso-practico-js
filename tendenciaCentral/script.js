07cb79062a96b6d8ef01f5b493db33965e542a5b
alexvillhen <alexvillhen@gmail.com>
2022-01-07 14:40:38 -0700

    añadida función de mediana

--- tendenciaCentral/script.js
+++ tendenciaCentral/script.js
@@ -1,9 +1,4 @@
 function promedio(arr){
-    /* let sumaArr = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sumaArr = sumaArr + arr[i];
-    } */
-
     const sumaArr = arr.reduce(
         function (valorAcumulado = 0, nuevoElemento){
             return valorAcumulado + nuevoElemento;
@@ -15,3 +10,38 @@ function promedio(arr){
 }
 
 console.log( promedio([1,2,3]) );
+
+
+//==========================================
+// MEDIANA
+
+const lista1 = [300,3,2,200,1000,10,1];
+
+function esPar(num) {
+    if(num % 2 == 0){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+
+function mediana(arr) {
+    const lista = arr.sort((a, b) => a - b);
+
+    const mitadArr = parseInt(lista.length/2);
+    
+    if (esPar(lista.length)){
+        const elemento1 = lista1[mitadArr];
+        const elemento2 = lista1[mitadArr - 1];
+        const mediana = promedio([elemento1, elemento2]);
+        return (mediana);
+    
+    } else {
+        const mediana = arr[mitadArr];
+        return mediana;
+    }
+}
+
+console.log( mediana(lista1) );
+
