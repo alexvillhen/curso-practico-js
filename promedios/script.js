cef392eb479ca276943c3081ad8cc2dfd58e68ac
alexvillhen <alexvillhen@gmail.com>
2022-01-27 17:22:59 -0700

    terminado algortimo promedio ponderado

--- promedios/script.js
+++ promedios/script.js
@@ -1,9 +1,42 @@
-const datos = {
-    10:4,
-    5:5,
-    7:8
+const datos = [
+    {
+        course: "Educación Física",
+        note: 10,
+        credit: 2,
+    },
+    {
+        course: "Programación",
+        note: 8,
+        credit: 5,
+    },
+    {
+        course: "Finanzas personales",
+        note: 7,
+        credit: 5,
+    },
+    {
+        course: "Finanzas personales",
+        note: 9.5,
+        credit: 2,
+    }
+];
+
+
+const promedioPondedaro = arr => {
+    const notesWithCredit = arr.map(course => course.note * course.credit);
+    const numerador = notesWithCredit.reduce((a, b) => a + b);
+
+    const creditSum = arr.map(course => course.credit);
+    const denominador = creditSum.reduce((a, b) => a + b);
+
+    const rta = numerador/denominador;
+    return rta;
 }
 
-for (var i = 0; i < datos.lenght; i++){
-    
-}
+console.log(promedioPondedaro(datos));
+
+
+
+
+
+
